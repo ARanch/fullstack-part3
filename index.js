c37abb683194bbe9d8896571d45b597dddd640b8
Andreas Ranch <ranch@sowasound.com>
2022-11-07 20:28:47 +0100

    3.3 done

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ app.get('/info', (request, response) => {
     response.send(payload)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    console.log('ID: ', request.params.id)
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    console.log('person is: ', person)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
